898062e2813663cda06a7704f9e932a908ffc38a
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2016-12-02 23:22:15 +0100

    get movie title and year and create symlinks to a testfile

--- src/main.py
+++ src/main.py
@@ -1,9 +1,43 @@
 import tmdbsimple as tmdb
-tmdb.API_KEY = 'your-api-key'
+import ConfigParser
+import time
+import os
+from datetime import datetime
 
+config = ConfigParser.RawConfigParser()
+config.read('movlibgen.cfg')
+x = 'abcdefghijklmnopqrstuvwxyz'
+path = config.get('CONFIG', 'PATH')
+samplefile = config.get('CONFIG', 'SAMPLE')
+tmdb.API_KEY = config.get('CONFIG', 'API_KEY')
 search = tmdb.Search()
-response = search.movie(query='fast', page='2')
-print (search.total_results)
-print (search.page)
-for s in search.results:
-	print(s['title'], s['release_date'])
+
+for i in x:
+	for p in range(1, 20):
+		response = search.movie(query=i, page=str(p), include_adult='no')
+		print (search.total_results)
+		print (search.page)
+		for s in search.results:
+			movie_year = ''
+			if s['release_date'] != '':
+				dt = datetime.strptime(s['release_date'], '%Y-%m-%d')
+				movie_year = dt.year
+			fakemovie = s['title']
+			fakemovie.replace(" ", "_")
+			fakemovie += "_"
+			fakemovie += str(movie_year)
+
+			fullsample = path
+			fullsample += "/"
+			fullsample += samplefile
+
+			fullfakemovie = path
+			fullfakemovie += "/"
+			fullfakemovie += fakemovie
+
+			print(s['title'], movie_year)
+			try:
+				os.symlink(fullsample, fullfakemovie)
+			except:
+				print "Could not create link, continue with next one..."
+		time.sleep(1)
