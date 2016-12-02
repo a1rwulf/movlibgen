eafc7b6801e96a89c2761c6d097478aabdaf4b5b
Wolfgang Haupt <haupt.wolfgang@gmail.com>
2016-12-02 23:40:11 +0100

    append mp4 extension and only skip 1 item if sth fails

--- src/main.py
+++ src/main.py
@@ -18,26 +18,27 @@ for i in x:
 		print (search.total_results)
 		print (search.page)
 		for s in search.results:
-			movie_year = ''
-			if s['release_date'] != '':
-				dt = datetime.strptime(s['release_date'], '%Y-%m-%d')
-				movie_year = dt.year
-			fakemovie = s['title']
-			fakemovie.replace(" ", "_")
-			fakemovie += "_"
-			fakemovie += str(movie_year)
-
-			fullsample = path
-			fullsample += "/"
-			fullsample += samplefile
-
-			fullfakemovie = path
-			fullfakemovie += "/"
-			fullfakemovie += fakemovie
-
-			print(s['title'], movie_year)
 			try:
+				movie_year = ''
+				if s['release_date'] != '':
+					dt = datetime.strptime(s['release_date'], '%Y-%m-%d')
+					movie_year = dt.year
+				fakemovie = str(s['title'])
+				fakemovie = fakemovie.replace(" ", "_")
+				fakemovie += "_"
+				fakemovie += str(movie_year)
+				fakemovie += ".mp4"
+
+				fullsample = path
+				fullsample += "/"
+				fullsample += samplefile
+
+				fullfakemovie = path
+				fullfakemovie += "/"
+				fullfakemovie += fakemovie
+
+				print(str(s['title']), movie_year)
 				os.symlink(fullsample, fullfakemovie)
 			except:
-				print "Could not create link, continue with next one..."
+				print "Skip to next"
 		time.sleep(1)
